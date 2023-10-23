5965f94f095541252d5512048049adb1ab6a1c82
Nazarov Ruslan <rnazarov142@gmail.com>
2023-10-23 18:52:42 +0200

    Добавьте файлы проекта.

---
+++ lab9_nazarov/Program.cs
@@ -0,0 +1,23 @@
+﻿//Вычисление бесконечных сумм, вариант 9.
+try
+{
+    Console.WriteLine("Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите n: ");
+    double n = double.Parse(Console.ReadLine());
+
+    double sum = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        long f = 1;
+        for (int j = 1; j <= 4 * i - 3; j++) f *= j;
+        sum += Math.Pow(x, 4 * i - 3) / f;
+        
+    }
+    Console.WriteLine($"S = {sum:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
